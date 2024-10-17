90546ddb04967b4a90d7c00286262fc7494b693a
Anurag <anuragsonkar053@gmail.com>
2024-10-17 07:40:27 +0530

    admin-dummy-login

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
+// handling only with frontend - as auth is not done at backend , focusing on main fucntionality that is showing user responses with there attachments
 function AdminLogin() {
+    const naigate = useNavigate()
+    const [formData , setFormData] = useState({
+        name : "",
+        password : ""
+
+    })
+
+    const handleChange = (e)=>{
+        setFormData({
+            ...formData , [e.target.name] : e.target.value
+        })
+    }
+
+    console.log(formData)
     const handleForm = (e) => {
         e.preventDefault()
-        const formData = new FormData(e.target)
-        const obj = Object.fromEntries(formData.entries())
+        
 
-        const images = formData.getAll('images');
-        obj.images = images;
-
-        console.log(obj)
+        if(formData.name !== '' && formData.password !== ''){
+            naigate('/')
+        }else{
+            alert('name & password required to login')
+            return
+        }
     }
   return (
       <div className='container'>
@@ -17,11 +34,11 @@ function AdminLogin() {
               <h1 className='heading'>Admin Login</h1>
               <div className='input-div'>
                   <label htmlFor='name'>username:</label>
-                  <input type='text' name='name' id='name' />
+                  <input type='text' name='name' id='name' value={formData.name}  onChange={handleChange}/>
               </div>
               <div className='input-div'>
                   <label htmlFor='password'>password:</label>
-                  <input type='password' name='password' id='password' />
+                  <input type='password' name='password' id='password' value={formData.password} onChange={handleChange}/>
               </div>
 
               <button type='submit' className='submit-btn'>Login</button>
