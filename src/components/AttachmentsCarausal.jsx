9c9cc60ea0ecc7a141744bfda2f75cb84891821a
Anurag <anuragsonkar053@gmail.com>
2024-10-17 09:33:10 +0530

    basic-carausal-added-for-more-attachmnets

--- src/components/AttachmentsCarausal.jsx
+++ src/components/AttachmentsCarausal.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GrNext, GrPrevious } from 'react-icons/gr'
 
 function AttachmentsCarausal({attachments }) {
+    const [currentIndex, setCurrentIndex] = useState(0); // For tracking the first visible image
+    const visibleAttachments = attachments.slice(currentIndex, currentIndex + 4); // Show only 4 attachments
+
     console.log(attachments)
+
   return (
       <div className='flex gap-3 flex-wrap items-center'>
-          <div className='cursor-pointer'><GrPrevious /></div>
-          {attachments?.map((attachment) => <img key={attachment._id} src={attachment.url} alt='attachment' className='w-48 h-auto rounded-md cursor-pointer' />)}
-          <div className='cursor-pointer'><GrNext /></div>
+          {currentIndex > 0 && (
+              <div className="cursor-pointer" onClick={() => setCurrentIndex(currentIndex - 1)}>
+                  <GrPrevious />
+              </div>
+          )}
+          {/* <GrPrevious onClick={() => setCurrentIndex(currentIndex - 1)} /> */}
+          <div className="flex gap-2 flex-wrap w-[58vw] justify-center transition-transform duration-3000 ease-in-out">
+              {visibleAttachments.map((attachment) => (
+                  <img
+                      key={attachment._id}
+                      src={attachment.url}
+                      alt="attachment"
+                      className="w-full max-w-44 max-h-36 rounded-md cursor-pointer object-contain"
+                  />
+              ))}
+          </div>
+          {currentIndex + 4 < attachments.length && (
+              <div className="cursor-pointer" onClick={() => setCurrentIndex(currentIndex + 1)}>
+                  <GrNext />
+              </div>
+          )}
+          {/* <GrNext onClick={() => setCurrentIndex(currentIndex + 1)} /> */}
       </div>
   )
 }
